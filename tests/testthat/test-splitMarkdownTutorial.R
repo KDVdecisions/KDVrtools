3cc6c89b117665101c580aa5c6c28904a1a85eb1
EmmaLiTsai <emmali.tsai@gmail.com>
2023-07-25 20:11:34 -0400

    another test added for splitRMDtutorial

--- tests/testthat/test-splitMarkdownTutorial.R
+++ tests/testthat/test-splitMarkdownTutorial.R
@@ -1,7 +1,18 @@
 test_that("Test to ensure that RMD with one header is split", {
-  splitMarkdownTutorial(test_path("testdata", "testsplitMarkdownTutorial.Rmd"))
+  splitMarkdownTutorial(test_path("testdata", "testsplitMarkdownTutorialSmall.Rmd"))
   expect_true(file.exists("CORE_ELEMENTS.R"))
   # cleaning up: 
   file.remove("CORE_ELEMENTS.R")
-  file.remove("testsplitMarkdownTutorial.R")
+  file.remove("testsplitMarkdownTutorialSmall.R")
+})
+
+test_that("Test to ensure that RMD with two headers is split", {
+  splitMarkdownTutorial(test_path("testdata", "testsplitMarkdownTutorialLarge.Rmd"))
+  expect_true(file.exists("R_Markdown.R"))
+  expect_true(file.exists("Including_Plots.R"))
+  
+  # cleaning up: 
+  file.remove("R_Markdown.R")
+  file.remove("Including_Plots.R")
+  file.remove("testsplitMarkdownTutorialLarge.R")
 })
